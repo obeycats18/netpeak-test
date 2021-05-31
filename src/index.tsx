31232048f3aac61e95be83c078151433522579af
KapishDima <dima.development@fakel.com.ua>
2021-05-31 08:45:01 +0300

    replace router to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 // Semantic UI Styles
 import "semantic-ui-css/semantic.min.css";
